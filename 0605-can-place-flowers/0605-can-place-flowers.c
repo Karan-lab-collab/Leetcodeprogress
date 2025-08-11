cfd5bb303617bd99e6edeea30ad945f9285c73fe
Karan-lab-collab <karanmittal987@gmail.com>
2025-08-11 11:36:42 +0530

    Time: 0 ms (100%), Space: 9.6 MB (1.14%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.c
@@ -0,0 +1,23 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    int x=0, flag=0;
+    for(int i=0; i<flowerbedSize; i++){
+        if(flowerbed[i]==1){
+            if(flag==1){
+                x--;
+            }
+            flag=-1;
+        }else{
+            if(flag==0){
+                x++;
+                flag=1;
+            }else{
+                flag=0;
+            }
+        }
+    }
+    if(x>=n){
+        return true;
+    }else{
+        return false;
+    }
+}
